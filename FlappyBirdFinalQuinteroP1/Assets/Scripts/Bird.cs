eadaffbba86916074d78d85a73ae686629a03d00
David Quintero <dquintero@dusd.net>
2023-12-08 13:54:43 -0800

    flappy example
    created part of the game

---
+++ FlappyBirdFinalQuinteroP1/Assets/Scripts/Bird.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce = 200f;
+
+    private bool isDead = false;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.Instance.BirdDied();
+    }
+}
---
+++ FlappyBirdFinalQuinteroP1/Assets/Scripts/GameControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl Instance;
+    public GameObject gameOverText;
+    public TextMeshProUGUI ScoreText;
+    //public TextMeshProUGUI Continue;
+
+    public bool gameOver = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver == true && Input.GetKeyUp(KeyCode.Space)) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
